4c812a104ad94fcc3159405bf9300de0e4b76a53
Katanuki Satoshi <katanukisatoshi@gmail.com>
2024-07-18 21:22:31 +0700

    Remove unused file and cache

--- apps/_old/image_saver.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import os
-
-class ImageSaver:
-    @staticmethod
-    def save_image(image, output_path, file_name):
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
-        cv2.imwrite(os.path.join(output_path, file_name), image)
